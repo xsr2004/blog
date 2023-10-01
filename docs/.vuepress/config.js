eb55499a9e2b29d50a7e8080a86936a8a619cd8b
xsr <2751247482@qq.com>
2023-10-01 20:02:06 +0800

    init respority

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,71 @@
+// vuePress的全局配置文件
+module.exports={
+    base:'/blog/',
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    markdown: {
+        lineNumbers: true
+    },
+    locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
+        '/': {
+            lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+            title: 'VuePress',
+            description: 'Vue-powered Static Site Generator'
+        },
+        '/zh/': {
+            lang: 'zh-CN',
+            title: 'VuePress',
+            description: 'Vue 驱动的静态网站生成器'
+        }
+    },
+    themeConfig: {
+        locales: {
+            '/': {
+                selectText: 'Languages',
+                label: 'English',
+                ariaLabel: 'Languages',
+                editLinkText: 'Edit this page on GitHub',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "New content is available.",
+                        buttonText: "Refresh"
+                    }
+                },
+                algolia: {},
+                nav: [
+                    {text: 'Nested', link: '/nested/', ariaLabel: 'Nested'}
+                ],
+                sidebar: {
+                    '/': [/* ... */],
+                    '/nested/': [/* ... */]
+                }
+            },
+            '/zh/': {
+                // 多语言下拉菜单的标题
+                selectText: '选择语言',
+                // 该语言在下拉菜单中的标签
+                label: '简体中文',
+                // 编辑链接文字
+                editLinkText: '在 GitHub 上编辑此页',
+                // Service Worker 的配置
+                serviceWorker: {
+                    updatePopup: {
+                        message: "发现新内容可用.",
+                        buttonText: "刷新"
+                    }
+                },
+                // 当前 locale 的 algolia docsearch 选项
+                algolia: {},
+                nav: [
+                    {text: '嵌套', link: '/zh/nested/'}
+                ],
+                sidebar: {
+                    '/zh/': [/* ... */],
+                    '/zh/nested/': [/* ... */]
+                }
+            }
+        }
+    }
+}
